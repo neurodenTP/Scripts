298460d624c066fbac4154329bb5c1ea63436a31
Nikita <67433712+NikomSol@users.noreply.github.com>
2025-12-19 16:44:05 +0300

    clean

--- 250926_python_my_lib/SNN_NK_V1/model_1_1.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-
-from neuron import LIFNeuron
-
-neuron_params = {}
-LIFNeuron(neuron_params)
